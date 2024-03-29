cb8ed23f9c0fb1c21c21895ed5a1d605b78cfa44
Dilson <dilsono316@gmail.com>
2024-03-29 11:28:36 -0300

    get fields in view

--- app/src/main/java/devandroid/dilson/applistaaluno/model/Student.java
+++ app/src/main/java/devandroid/dilson/applistaaluno/model/Student.java
@@ -1,4 +1,51 @@
 package devandroid.dilson.applistaaluno.model;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String courseName;
+    private String numberCod;
+
+    public Student(){
+
+    }
+
+    public Student(String firstName, String lastName, String courseName, String numberCod) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.courseName = courseName;
+        this.numberCod = numberCod;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getNumberCod() {
+        return numberCod;
+    }
+
+    public void setNumberCod(String numberCod) {
+        this.numberCod = numberCod;
+    }
 }
--- app/src/main/java/devandroid/dilson/applistaaluno/view/MainActivity.java
+++ app/src/main/java/devandroid/dilson/applistaaluno/view/MainActivity.java
@@ -1,6 +1,9 @@
 package devandroid.dilson.applistaaluno.view;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import devandroid.dilson.applistaaluno.R;
+import devandroid.dilson.applistaaluno.model.Student;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText txtFirstName;
+    EditText txtLastName;
+    EditText txtCourseName;
+    EditText txtNumberCod;
+
+    Button buttonReset;
+    Button buttonSave;
+    Button buttonFinish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +34,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Student student = new Student("Dilson","Campêlo","Java","12211");
+        txtFirstName = findViewById(R.id.txtFirstName);
+        txtLastName = findViewById(R.id.txtLastName);
+        txtCourseName = findViewById(R.id.txtCourseName);
+        txtNumberCod = findViewById(R.id.txtNumberCod);
+
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonSave = findViewById(R.id.buttonSave);
+        buttonFinish = findViewById(R.id.buttonFinish);
+
+        txtFirstName.setText(student.getFirstName());
+        txtLastName.setText(student.getLastName());
+        txtCourseName.setText(student.getCourseName());
+        txtNumberCod.setText(student.getNumberCod());
+
+        Log.i("Poo","Aqui");
     }
 }

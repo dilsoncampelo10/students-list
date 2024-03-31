8ad47f2b4c04e8edeb93b8168ae9ac2bd8488ae1
Dilson <dilsono316@gmail.com>
2024-03-31 17:37:30 -0300

    create splash activity

---
+++ app/src/main/java/devandroid/dilson/applistaaluno/view/SplashActivity.java
@@ -0,0 +1,42 @@
+package devandroid.dilson.applistaaluno.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import devandroid.dilson.applistaaluno.R;
+
+public class SplashActivity extends AppCompatActivity {
+    public static final int TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        changeToMainView();
+    }
+
+    private void changeToMainView() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        },TIME_OUT);
+
+    }
+}
